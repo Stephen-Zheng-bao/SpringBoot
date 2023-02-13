b1ab66c8c1ef3ff00489c300c6a0ff2b89807f4e
warrenorme <warrenorme@gmail.com>
2023-02-13 20:43:45 +0000

    Spring Security Works

--- src/main/java/com/example/demo/User/UserRepository.java
+++ src/main/java/com/example/demo/User/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.demo.User;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+	Optional<User> findByName(String username);
 
 
 }
--- src/main/java/com/example/demo/User/UserService.java
+++ src/main/java/com/example/demo/User/UserService.java
@@ -1,16 +1,18 @@
 package com.example.demo.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-
+	@Autowired
     private final UserRepository userRepository;
+	@Autowired
+    private PasswordEncoder passwordEncoder;
 
-    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -22,6 +24,7 @@ public class UserService {
     }
 
     public User createUser(User user) {
+    	user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
